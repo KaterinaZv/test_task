bf60a9b4550d61b28502703eb0127fe6ee0d553d
Ekaterina Zvereva <katuha.hip.hop@gmail.com>
2020-10-29 16:37:21 +0300

    add FakeApiService

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  parser: "babel-eslint",
+  extends: ["standard", "plugin:prettier/recommended"],
+  env: {
+    browser: true,
+    es6: true,
+  },
+};
--- index.js
+++ index.js
@@ -1,131 +1,222 @@
 "use strict";
 
-const URL_DATABASE = "http://127.0.0.1:5500/server/db.json";
-const URL_SUCCESS = "/server/success.json";
-const URL_ERROR = "/server/error.json";
-const URL_PROGRESS = "/server/progress.json";
 const resultContainer = document.querySelector("#resultContainer");
 
-const url = "http://127.0.0.1:5500/db";
+class Validation {
+  constructor() {
+    this.emailRegex = /^[a-zA-Z0-9_.]+@(gmail\.com|(yandex|mail)\.ru)$/;
+    this.phoneRegex = /\+7\(\d{3}\)\d{3}-\d{2}-\d{2}/;
+  }
+
+  validateEmail(email) {
+    const isEmailValid = this.emailRegex.test(email);
+    if (isEmailValid) {
+      return undefined;
+    }
+
+    return "Необходимо указать домен yandex.ru, mail.ru или gmail.com.";
+  }
+
+  validatePhone(phone) {
+    const resultTestPhone = this.phoneRegex.test(phone);
+
+    const countOfNumbersInPhone = phone
+      .split("")
+      .map((string) => parseInt(string))
+      .filter((value) => value)
+      .reduce((accumulator, phoneNum) => accumulator + phoneNum, 0);
+
+    const isPhoneValid = resultTestPhone && countOfNumbersInPhone < 30;
+    if (isPhoneValid) {
+      return undefined;
+    }
+
+    return "Телефон должен быть указан в формате +7(999)999-99-99";
+  }
+
+  validateName(name) {
+    const isNameValid = name.split(" ").length === 3;
+    if (isNameValid) {
+      return undefined;
+    }
+
+    return "Должно быть 3 слова";
+  }
+}
+
+class FakeApiService {
+  _get(url) {
+    return new Promise((resolve) =>
+      setTimeout(() => {
+        const randomNumber = Math.random();
+        const randomIsSuccess = randomNumber <= 0.33;
+        const randomIsError = randomNumber > 0.33 && randomNumber <= 0.66;
+        const randomIsProgress = randomNumber > 0.66;
+
+        if (randomIsSuccess) {
+          return resolve({ status: "success" });
+        }
+
+        if (randomIsError) {
+          resolve({ status: "error", reason: "Something went wrong" });
+        }
+
+        if (randomIsProgress) {
+          resolve({ status: "progress", timeout: Number });
+        }
+      }, 1500)
+    );
+  }
+
+  async sendRequest(url, onSuccess, onError) {
+    const fakeResponse = await this._get(url);
+
+    if (fakeResponse.status === "success") {
+      onSuccess(fakeResponse);
+    } else {
+      onError(fakeResponse);
+    }
+  }
+}
 
 class MyForm {
   constructor(idForm) {
-    this.form = document.querySelector(`#${idForm}`);
-    this.name = document.querySelector(".fio");
-    this.email = document.querySelector(".email");
-    this.phone = document.querySelector(".phone");
+    this.formElement = document.querySelector(`#${idForm}`);
+    this.nameInput = document.querySelector(".fio");
+    this.emailInput = document.querySelector(".email");
+    this.phoneInput = document.querySelector(".phone");
+
+    this.buttonElement = this.formElement.querySelector("#submitButton");
+
+    this.formElement.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      this.submit();
+    });
+
+    this.submit = this.submit.bind(this);
+
+    this._onRequestSuccess = this._onRequestSuccess.bind(this);
+    this._onRequestFail = this._onRequestFail.bind(this);
   }
 
   validate() {
     const data = this.getData();
-    let resultName, resultPhone, resultEmail;
-    let errorFields = [];
 
-    // Проверка на валидацию имени
-    if (data.fio.split(" ").length === 3) {
-      resultName = data.fio;
-    } else {
-      this.name.classList.add("error");
-      this.name.value = "";
-      this.name.placeholder = "Ровно Три Слова";
+    const errorFields = [];
+
+    const validation = new Validation();
+    const validationErrors = {
+      name: validation.validateName(data.fio),
+      email: validation.validateEmail(data.email),
+      phone: validation.validatePhone(data.phone),
+    };
+
+    if (validationErrors.name) {
+      this.nameInput.classList.add("error");
+      this.nameInput.value = "";
+      this.nameInput.placeholder = validationErrors.name;
       errorFields.push("fio");
     }
 
-    // Проверка на валидацию email
-    const reg = /^[a-zA-Z0-9_.]+@(gmail\.com|(yandex|mail)\.ru)$/;
-    const resultTestEmail = reg.test(data.email);
-    if (resultTestEmail) {
-      resultEmail = data.email;
-    } else {
-      this.email.classList.add("error");
-      this.email.value = "";
-      this.email.placeholder =
-        "Только в доменах yandex.ru, mail.ru, gmail.com.";
+    if (validationErrors.email) {
+      this.emailInput.classList.add("error");
+      this.emailInput.value = "";
+      this.emailInput.placeholder = validationErrors.email;
       errorFields.push("email");
     }
 
-    // Проверка на валидацию номера
-    const regPhone = /\+7\(\d{3}\)\d{3}-\d{2}-\d{2}/;
-    const resultTestPhone = regPhone.test(data.phone);
-
-    const sumNum = data.phone
-      .split("")
-      .map((string) => parseInt(string))
-      .filter((value) => value)
-      .reduce(sum, 0);
-
-    function sum(accumulator, a) {
-      return accumulator + a;
-    }
-
-    if (resultTestPhone && sumNum < 30) {
-      resultPhone = data.phone;
-    } else {
-      this.phone.classList.add("error");
-      this.phone.value = "";
-      this.phone.placeholder = "В формате +7(999)999-99-99";
+    if (validationErrors.phone) {
+      this.phoneInput.classList.add("error");
+      this.phoneInput.value = "";
+      this.phoneInput.placeholder = validationErrors.phone;
       errorFields.push("phone");
     }
 
-    if (resultPhone && resultName && resultEmail) {
+    const isFormVaild =
+      !validationErrors.name &&
+      !validationErrors.email &&
+      !validationErrors.phone;
+
+    if (isFormVaild) {
       return {
         isValid: true,
         errorField: 0,
       };
-    } else {
-      return {
-        isValid: false,
-        errorField: errorFields,
-      };
     }
+
+    return {
+      isValid: false,
+      errorField: errorFields,
+    };
   }
 
   getData() {
-    let data;
-    data = {
-      fio: this.name.value,
-      email: this.email.value,
-      phone: this.phone.value,
+    return {
+      fio: this.nameInput.value,
+      email: this.emailInput.value,
+      phone: this.phoneInput.value,
     };
-    return data;
   }
 
   setData(data) {
-    data = {
-      fio: (this.name.value = data.fio),
-      email: (this.email.value = data.email),
-      phone: (this.phone.value = data.phone),
-    };
+    this.nameInput.value = data.fio;
+    this.emailInput.value = data.email;
+    this.phoneInput.value = data.phone;
   }
 
   submit() {
-    const button = this.form.querySelector("#submitButton");
     if (this.validate().isValid) {
-      button.disabled = true;
-      this.form.reset();
-      this.sendRequest(URL_SUCCESS).then(
-        (data) => (resultContainer.innerText = data.status)
-      );
-    } else {
-      this.sendRequest(URL_ERROR).then(
-        (data) => (resultContainer.innerHTML = data.status)
+      this._disableForm();
+      const formEndpointUrl = this.formElement.getAttribute("action");
+
+      const apiService = new FakeApiService();
+      apiService.sendRequest(
+        formEndpointUrl,
+        this._onRequestSuccess,
+        this._onRequestFail
       );
     }
   }
 
-  // Отправка запроса
-  sendRequest(url) {
-    return fetch(url, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }).then((res) => res.json());
+  _disableForm() {
+    this.buttonElement.disabled = true;
+    this.emailInput.disabled = true;
+    this.phoneInput.disabled = true;
+    this.nameInput.disabled = true;
+  }
+
+  _activateForm() {
+    this.buttonElement.disabled = false;
+    this.emailInput.disabled = false;
+    this.phoneInput.disabled = false;
+    this.nameInput.disabled = false;
+  }
+
+  _polling() {
+    setTimeout(this.submit, 1500);
+  }
+
+  _onRequestSuccess(data) {
+    this.buttonElement.disabled = false;
+
+    resultContainer.innerText = data.status;
+
+    this._activateForm();
+  }
+
+  _onRequestFail(data) {
+    if (data.status === "progress") {
+      this._polling();
+    } else {
+      this._activateForm();
+    }
+
+    this.buttonElement.disabled = false;
+    resultContainer.innerHTML = data.status;
   }
 }
 
-document.addEventListener("submit", (e) => {
-  e.preventDefault();
+(function () {
   const form = new MyForm("data");
-  form.submit();
-});
+})();
